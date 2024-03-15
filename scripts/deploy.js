99d88f4e60364aae9fbb8776af8d5a168ee99947
jonofficial <jonrocky10@gmail.com>
2024-03-16 02:11:42 +0530

    added code

---
+++ scripts/deploy.js
@@ -0,0 +1,13 @@
+const hre=require("hardhat");
+async function main() {
+    const Lock=await hre.ethers.getContractFactory("Lock");
+    const lock=await Lock.deploy();
+
+    await lock.deployed();
+    console.log("Lock with 1 ETH deployed to:",lock.address);   
+}
+
+main().catch((error)=>{
+    console.error(error);
+    process.exit(1);
+});
